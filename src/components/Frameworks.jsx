b86eb5cee084700735318b1d5e25115a23d335be
Shashant Kashyap <shashant.kashyap999@gmail.com>
2025-11-30 17:57:30 +0530

    first commit

---
+++ src/components/Frameworks.jsx
@@ -0,0 +1,38 @@
+import { OrbitingCircles } from "./OrbitingCircles";
+
+export function Frameworks() {
+  const skills = [
+    "auth0",
+    "Node.js",
+    "Next",
+    "AWS",
+    "Docker",
+    "git",
+    "JS-logo",
+    "Postgress",
+    "react",
+    "SQLite",
+    "Tailwind",
+    "Vite.js",
+    "github",
+    "TypeScript"
+  ];
+  return (
+    <div className="relative flex h-60 w-full flex-col items-center justify-center">
+      <OrbitingCircles iconSize={40}>
+        {skills.map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.png`} />
+        ))}
+      </OrbitingCircles>
+      <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
+        {skills.reverse().map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.png`} />
+        ))}
+      </OrbitingCircles>
+    </div>
+  );
+}
+
+const Icon = ({ src }) => (
+  <img src={src} className="duration-200 rounded-sm hover:scale-110" />
+);
---
+++ src/constants/index.js
@@ -0,0 +1,243 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Whats New",
+    description:
+      "AI Driven News Content Plateform",
+    subDescription: [
+      "Developed a microservice-based backend system with dedicated User and Content services for scalable news content generation.",
+" Integrated Google Gemini API to dynamically generate and summarize topic-specific news through a scheduled AI content pipeline.",
+ "Built secure authentication and user management with JWT, cookie-based sessions, OTP validation, and NodeMailer for verification.",
+ "Managed dual databases (MySQL + MongoDB) for structured and unstructured data, implementing migrations for schema updates."
+    ],
+    href: "https://github.com/Shashantkashyap/Whats_New",
+    logo: "",
+    image: "/assets/projects/Whats_New.png",
+    tags: [
+      {
+        id: 1,
+        name: "Node",
+        path: "/assets/logos/Node.js.png",
+      },
+      {
+        id: 2,
+        name: "SQL/MongoDB",
+        path: "/assets/logos/SQLite.png",
+      },
+      {
+        id: 3,
+        name: "Express",
+        path: "/assets/logos/Express.png",
+      },
+      {
+        id: 4,
+        name: "Gemini-API",
+        path: "/assets/logos/Google.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Pulse Chat",
+    description:
+      "Social Media Plateform Designed for 1:1 chating",
+    subDescription: [
+       
+ "Engineered a full-stack MERN chat application with real-time 1:1 and group messaging using WebSocket, ensuring low-latency communication.",
+
+" Enhanced user experience with customizable UI themes and optimized backend performance for scalability and reliability.",
+ 
+ "Planned feature expansion with WebRTC (UDP) for video calling, showcasing forward-thinking architecture design."
+    ],
+    href: "https://github.com/Shashantkashyap/Pulse-Chat",
+    logo: "",
+    image: "/assets/projects/Pulse_Chat.png",
+    tags: [
+      {
+        id: 1,
+        name: "WebSocket",
+        path: "/assets/logos/Socket.io.png",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.png",
+      },
+      {
+        id: 3,
+        name: "SQLite",
+        path: "/assets/logos/MongoDB.png",
+      },
+      {
+        id: 4,
+        name: "Node",
+        path: "/assets/logos/Node.js.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Entertainment",
+    description:
+      "Fun loving movie Description website",
+    subDescription: [
+      "Developed an entertainment web application using the MERN stack (MongoDB, Express.js, React, Node.js) for seamless movie and TV show exploration.",
+ "Integrated TMDB, IMDB, and YouTube APIs to deliver rich content details, ratings, and trailers within an interactive interface.",
+ "Implemented secure JWT & bcrypt-based authentication and bookmarking features, enhancing personalization, security, and user engagement."
+    ],
+    href: "https://github.com/Shashantkashyap/Entertainment",
+    logo: "",
+    image: "/assets/projects/Entertainment.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.png",
+      },
+      {
+        id: 2,
+        name: "Node",
+        path: "/assets/logos/Node.js.png",
+      },
+      {
+        id: 3,
+        name: "SQLite",
+        path: "/assets/logos/MongoDB.png",
+      },
+      
+    ],
+  },
+  {
+    id: 4,
+    title: "Tyrebook",
+    description:
+      "TyreBook ERP is a comprehensive tyre management solution designed specifically for the transportation industry.",
+    subDescription: [
+"Complete Fleet Management: Track vehicles, trailers, and tyres in one unified system",
+"End-to-End Tyre Lifecycle: Manage from procurement to disposal",
+"Real-Time Tracking: Monitor tyre location, condition, and performance",
+"Inspection Management: Schedule and track regular vehicle inspections"
+    ],
+    href: "https://www.tyrebook.in/",
+    logo: "",
+    image: "/assets/projects/Tyrebook.png",
+    tags: [
+      {
+        id: 1,
+        name: "Node",
+        path: "/assets/logos/Node.js.png",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.png",
+      },
+      {
+        id: 3,
+        name: "SQL",
+        path: "/assets/logos/SQLite.png",
+      },
+      {
+        id: 4,
+        name: "AWS",
+        path: "/assets/logos/AWS.png",
+      },
+    ],
+  },
+ 
+  
+];
+
+export const mySocials = [
+ 
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/shashant-kashyap-95a909388/",
+    icon: "/assets/socials/linkedIn.svg",
+  }
+];
+
+export const experiences = [
+  {
+    title: "Full Stack Developer",
+    job: "Job Seeking Plateform",
+    date: "2023-2024",
+    contents: [
+       
+ "Developed a full-stack MERN web application with dual user roles (Job Seeker & Employer), enabling seamless interaction between candidates and recruiters.",
+ 
+ "Delivered end-to-end functionality while independently learning new technologies, demonstrating adaptability and rapid skill acquisition"
+    ],
+  },
+  {
+    title: "Software Developer",
+    job: "Tyrebook ERP",
+    date: "2024-2025",
+    contents: [
+       "Developed a web application from scratch to industry-ready status using the MERN stack (MongoDB, Express.js, React.js, Node.js) and SQL.",
+ "Utilized AWS for cloud deployment, resource optimization, and scalability.",
+ "Focused on delivering a robust, production-ready application within project deadlines.",
+" Gained experience in designing, building, and deploying a scalable, user-focused web solution.",
+" Providing AI and automation-driven solutions to enhance efficiency and scalability. "
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2023-Present",
+    contents: [
+"       Completed 15+ freelance projects, including one for DOIT, Govt. of Rajasthan.",
+" Built MERN and Node.js applications with AI integration (Gemini & OpenAI APIs).",
+" Delivered secure, scalable, and production-ready solutions"    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
